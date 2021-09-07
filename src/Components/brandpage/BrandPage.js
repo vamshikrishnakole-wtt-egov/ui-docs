35ed04b491df660640512cc7967783e2d9d7d278
hariom-egov <hariom.p@egovernments.org>
2021-09-07 15:59:35 +0530

    page content changes

--- src/Components/brandpage/BrandPage.js
+++ src/Components/brandpage/BrandPage.js
@@ -16,7 +16,7 @@ const BrandPage = () => {
       Image: MissionImg,
       para: "Our mission is to unleash the potential in every team.",
       linkContent: "Explore",
-      linkpath : "/brand/mission"
+      linkpath: "/brand/mission",
     },
     {
       id: 2,
@@ -24,7 +24,7 @@ const BrandPage = () => {
       Image: PersonalityImg,
       para: "Our personality describes the tone we use for external communications. It should be expressed as an embodiment of our values.",
       linkContent: "Explore",
-      linkpath : "/brand/personality"
+      linkpath: "/brand/personality",
     },
     {
       id: 3,
@@ -32,7 +32,7 @@ const BrandPage = () => {
       Image: PromiseImg,
       para: "Our promise is that our tools and practices will help teams work better together in an agile, open, and scalable way.",
       linkContent: "Explore",
-      linkpath : "/brand/promise"
+      linkpath: "/brand/promise",
     },
     {
       id: 4,
@@ -40,7 +40,7 @@ const BrandPage = () => {
       Image: ValuesImg,
       para: "Our values reflect the spirit with which DIGIT works to fulfill our promise.",
       linkContent: "Explore",
-      linkpath : "/brand/values"
+      linkpath: "/brand/values",
     },
   ];
   return (
@@ -50,31 +50,35 @@ const BrandPage = () => {
       </div>
       <div className="pb-4">
         <p className="md:text-xs lg:text-sm 2xl:text-base py-2">
-          Our brand reflects who we are and how we want our users to feel when
-          they use our products. It’s the unique combination of our mission and
-          values, and principles that drive our promise to unleash the potential
-          of every team.
+          Brand visualization is all about the consistent use of colours,
+          patterns, and components. We have assimilated the key design
+          principles and standards that contribute to the making of the DIGIT
+          brand.
         </p>
       </div>
       <div className="grid grid-cols-1 2xl:gap-8 gap-4 sm:grid-cols-2">
         {CardData.map((item) => {
           return (
             <Card key={item.id}>
-              <div className="p-8">
-                <img src={item.Image} alt={item.heading} className="w-16" />
-                <h2 className="py-3 2xl:text-2xl sm:text-xl">{item.heading}</h2>
-                <p className="md:text-xs lg:text-sm 2xl:text-base">
-                  {item.para}
-                </p>
-                <Link
-                  to={item.linkpath}
-                  className="text-border xl:text-base md:text-sm sm:text-xs flex items-center"
-                >
-                  {item.linkContent}{" "}
-                  <span>
-                    <img src={arrowSign} alt="->" className="w-4 pl-1" />
-                  </span>
-                </Link>
+              <div className="p-8 flex">
+                <img src={item.Image} alt={item.heading} />
+                <div className="pl-4">
+                  <h2 className="py-3 2xl:text-2xl sm:text-xl">
+                    {item.heading}
+                  </h2>
+                  <p className="md:text-xs lg:text-sm 2xl:text-base">
+                    {item.para}
+                  </p>
+                  <Link
+                    to={item.linkpath}
+                    className="text-border xl:text-base md:text-sm sm:text-xs flex items-center"
+                  >
+                    {item.linkContent}{" "}
+                    <span>
+                      <img src={arrowSign} alt="->" className="w-4 pl-1" />
+                    </span>
+                  </Link>
+                </div>
               </div>
             </Card>
           );
