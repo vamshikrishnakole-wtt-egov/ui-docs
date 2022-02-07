66ef97420ca98008d8cf0f5bfad351ff776d4ede
Nipun Arora <aroranipun1@gmail.com>
2022-02-07 14:34:44 +0530

    added iconography page

--- src/Components/foundationspage/Iconography.js
+++ src/Components/foundationspage/Iconography.js
@@ -11,6 +11,16 @@ import {
   Header,
 } from "@egovernments/digit-ui-react-components";
 
+import actionImg from '../../assets/img/foundations/iconography/action.jpg'
+import communicationImg from '../../assets/img/foundations/iconography/communication.jpg'
+import alertImg from '../../assets/img/foundations/iconography/alert.jpg'
+import mapsImg from '../../assets/img/foundations/iconography/maps.jpg'
+import socialImg from '../../assets/img/foundations/iconography/social.jpg'
+import notificationImg from '../../assets/img/foundations/iconography/notification.jpg'
+import imageImg from '../../assets/img/foundations/iconography/image.jpg'
+import fileImg from '../../assets/img/foundations/iconography/file.jpg'
+import contentImg from '../../assets/img/foundations/iconography/content.jpg'
+
 const Iconography = () => {
   return (
     <div className="md:w-5/6 w-full mb-60">
@@ -25,7 +35,7 @@ const Iconography = () => {
           of the design system.
         </p>
       </div>
-      <div className="grid grid-cols-2 gap-4">
+      {/* <div className="grid grid-cols-2 gap-4">
         <div className="py-2">
           <div className="grid grid-cols-10">
             <AnnouncementIcon />
@@ -39,7 +49,77 @@ const Iconography = () => {
         <div className="grid grid-cols-10">
           <SearchIconSvg />
         </div>
+      </div> */}
+      {/* <div className="grid grid-cols-2">
+        
+          <img className="object-none object-left-top" src={actionImg} alt="actions" />
+          <br />
+          <img className="object-none object-left" src={notificationImg} alt="actions" />
+          <br />
+          <img className="object-none object-left" src={socialImg} alt="actions" />
+          <br />
+          <img className= "object-none object-left-bottom" src={mapsImg} alt="actions" />
+          <br/>
+        
+        
+          <img className="object-none object-right-top" src={alertImg} alt="actions" />
+          <br />
+          <img className="object-none object-right" src={communicationImg} alt="actions" />
+          <br />
+          <img className="object-none object-right" src={contentImg} alt="actions" />
+          <br />
+          <img className="object-none object-right" src={fileImg} alt="actions" />
+          <br />
+          <img className="object-none object-right-bottom" src={imageImg} alt="actions" />
+          <br />
+        
+      </div> */}
+    
+      {/* <div className="grid grid-cols-2">
+        
+          <img className="p-4 flex flex-row h-32" src={actionImg} alt="actions" />
+          <img className="p-4 flex flex-row h-32" src={notificationImg} alt="actions" />
+          <img className="p-4 flex flex-row h-32" src={socialImg} alt="actions" />
+          <img className="p-4 flex flex-row h-32" src={mapsImg} alt="actions" />
+  
+          <img className="p-4 flex flex-col h-32" src={alertImg} alt="actions" />
+          <img className="p-4 flex flex-col h-32" src={communicationImg} alt="actions" />
+          <img className="p-4 flex flex-col h-32" src={contentImg} alt="actions" />
+          <img className="p-4 flex flex-col h-32" src={fileImg} alt="actions" />
+          <img className="p-4 flex flex-col h-32" src={imageImg} alt="actions" />
+        
+      </div> */}
+
+      {/* <div className="grid grid-cols-2">
+        
+          <img className="p-1 flex flex-row h-32" src={actionImg} alt="actions" />
+          <img className="p-1 flex flex-col h-32" src={alertImg} alt="actions" />
+          <img className="p-1 flex flex-row h-32" src={notificationImg} alt="actions" />
+          <img className="p-1 flex flex-col h-32" src={communicationImg} alt="actions" />
+          <img className="p-1 flex flex-row h-32" src={socialImg} alt="actions" />
+          <img className="p-1 flex flex-col h-32" src={contentImg} alt="actions" />          
+          <img className="p-1 flex flex-row h-32" src={mapsImg} alt="actions" />
+          <img className="p-1 flex flex-col h-32" src={fileImg} alt="actions" />
+          
+          <img className="p-1 flex flex-col h-32" src={imageImg} alt="actions" />
+        
+      </div> */}
+
+      <div className="grid grid-cols-2 gap-4">
+        
+          <img className="p-2 row-span-4" src={actionImg} alt="actions" />
+          <img className="p-2" src={alertImg} alt="actions" />
+          <img className="p-2" src={communicationImg} alt="actions" />
+          <img className="p-2" src={contentImg} alt="actions" />
+          <img className="p-2" src={fileImg} alt="actions" />        
+          <img className="p-2" src={notificationImg} alt="actions" />
+          <img className="p-2 row-span-2" src={imageImg} alt="actions" />
+          <img className="p-2" src={socialImg} alt="actions" />
+
+          <img className="p-2" src={mapsImg} alt="actions" />
+
       </div>
+
     </div>
   );
 };
